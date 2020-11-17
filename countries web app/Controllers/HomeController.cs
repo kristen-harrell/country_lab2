87789857759ce89958b67b337b1a1c4abd77075c
Kristen Harrell <harrell.kristen@gmail.com>
2020-11-17 09:42:58 -0500

    first commit

---
+++ countries web app/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using countries_web_app.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace countries_web_app.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Country> CountryList = new List<Country>
+        {
+            new Country("Kenya", "this is the kenya language", "hello world greeting", "description of kenya", "black, red, green"),
+            new Country("Jamaica", "this is the jamaica language", "hello world greeting", "description of jamaica", "green, yellow, black"),
+            new Country("Colombia", "this is the colombia language", "hello world greeting", "description of colombia", "yellow, blue, red"),
+            };
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CountryDetails()
+        {
+            return View();
+        }
+
+        public IActionResult Countries()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Countries(string country)
+        {
+            HttpContext.Session.SetString("countrydescription", country);
+            ViewBag.sess = HttpContext.Session.GetString("countrydescription");
+            //Country c = CountryList[index];
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CountrySelection(string country)
+        {
+            return View();
+        }
+        public IActionResult CountryDescription()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ countries web app/Models/Country.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace countries_web_app.Models
+{
+    public class Country
+    {
+        public string Name { get; set; }
+        public string OfficialLanguages { get; set; }
+        public string Greeting { get; set; }
+        public string Description { get; set; }
+        public string Colors { get; set; }
+
+        public Country() { }
+        public Country(string name, string officialLanguage, string greeting, string description, string colors)
+        {
+            this.Name = name;
+            this.OfficialLanguages = officialLanguage;
+            this.Greeting = greeting;
+            this.Description = description;
+            this.Colors = colors;
+        }
+    }
+}
